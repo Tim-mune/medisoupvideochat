061e5081a2b6af1c3f463b1e1938f7ee9f673e2f
Tim <neshtimo38@gmail.com>
2025-08-01 12:55:46 +0300

    updates

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,8 @@ import https from "https";
 import express from "express";
 import { Server } from "socket.io";
 import { createWorkers } from "./worker";
+import { Client } from "./classes/Client";
+import { Room } from "./classes/Room";
 
 const app = express();
 
@@ -17,10 +19,28 @@ const io = new Server(httpsServer, {
   },
 });
 
-const { router, worker } = await createWorkers();
+const { router } = await createWorkers();
 
 io.on("connection", (socket) => {
+  let client: Client;
   console.log("a user connected");
+
+  socket.on("joinRoom", ({ roomName, userName }, ack) => {
+    client = new Client(userName, roomName, router);
+    const room = new Room(router);
+    room.addClient(userName, client);
+    ack(router.rtpCapabilities);
+  });
+
+  socket.on("createTransport", async (ack) => {
+    try {
+      const transportParams = await client.createTransport();
+      transportParams;
+      ack(transportParams);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
 
 httpsServer.listen(3001, () =>
--- src/state/appstate.ts
+++ src/state/appstate.ts
@@ -1,17 +1,26 @@
 import { Device } from "mediasoup-client";
+import { Transport } from "mediasoup-client/types";
+import toast from "react-hot-toast";
 import { Socket, io } from "socket.io-client";
 import { create } from "zustand";
 
 interface AppState {
-  joinRoom: (data: { room: string; name: string }) => void;
+  joinRoom: (data: { roomName: string; userName: string }) => Promise<string>;
+  // getUserMedia:()=>
   socket: Socket | null;
   device: Device | null;
+  transport: Transport | null;
+  localStream: MediaStream | null;
+  setLocalStream: (stream: MediaStream) => void;
   initSocketIO: () => Socket;
+  createTansport: () => void;
 }
 
-export const useAppState = create<AppState>((set) => ({
+export const useAppState = create<AppState>((set, get) => ({
   socket: null,
   device: null,
+  transport: null,
+  localStream: null,
 
   initSocketIO() {
     const socket = io("https://localhost:3001", {
@@ -22,7 +31,56 @@ export const useAppState = create<AppState>((set) => ({
     return socket;
   },
 
-  joinRoom({ room, name }) {
-    console.log("Joining room:", room, "as", name);
+  setLocalStream(stream) {
+    set({ localStream: stream });
+  },
+
+  async joinRoom(data) {
+    const { socket } = get();
+    if (!socket) {
+      toast.error("Reload your browser tab");
+      return "Error";
+    }
+    try {
+      const res = await socket?.emitWithAck("joinRoom", data);
+      console.log("res", res);
+
+      const device = new Device();
+      await device.load({
+        routerRtpCapabilities: res,
+        preferLocalCodecsOrder: true,
+      });
+      set({ device });
+      return data.roomName;
+    } catch (error) {
+      console.error("err", error);
+      return "Error";
+    }
+  },
+
+  async createTansport() {
+    const { device, socket, localStream } = get();
+    if (!socket) {
+      toast.error("Please reload your browser");
+      return;
+    }
+
+    const transportParams = await socket?.emitWithAck("createTransport");
+    console.log("transport", transportParams);
+
+    if (!transportParams) {
+      toast.error("Server Failed WebRTC Transport");
+    }
+    const transport = device?.createSendTransport(transportParams);
+    transport?.on("connect", ({ dtlsParameters }, callback, errorback) =>
+      console.log(`connected!!`, dtlsParameters)
+    );
+    transport?.on("produce", ({ kind, rtpParameters }, callback, errorback) =>
+      console.log(`produce`, kind, rtpParameters)
+    );
+    set({ transport });
+    const tracks = localStream?.getTracks();
+    tracks?.forEach(async (track) => await transport?.produce({ track }));
+    console.log("send transport created successfully!", transport);
   },
 }));
